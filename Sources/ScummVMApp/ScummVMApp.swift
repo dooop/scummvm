512beb97cea98237e1313e6f77306d073374b00d
Dominic Opitz <dominic.opitz@gmail.com>
2026-02-20 23:42:44 +0100

    feat: implement ScummVMApp macOS executable with SwiftUI entry point

---
+++ Sources/ScummVMApp/ScummVMApp.swift
@@ -0,0 +1,30 @@
+//
+//  ScummVM.swift
+//  ScummVM
+//
+//  Created by Dominic Opitz on 2/14/26.
+//
+
+#if os(macOS)
+  import SwiftUI
+  import ScummVM
+
+  @main
+  struct ScummVMApp: App {
+    var body: some Scene {
+      WindowGroup {
+        ScummVM()
+          .frame(minWidth: 800, minHeight: 600)
+      }
+    }
+  }
+#else
+  import Foundation
+
+  @main
+  enum ScummVMApp {
+    static func main() {
+      fputs("ScummVMTestApp is only supported on macOS.\n", stderr)
+    }
+  }
+#endif
